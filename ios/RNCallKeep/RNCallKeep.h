5087b2a635f44535ec6d5721fc189161a3a92f0b
Sergii.Kliuchnyk <sergii.kliuchnyk@masterofcode.com>
2020-09-29 11:55:19 +0300

    fix reportNewIncomingCall

--- ios/RNCallKeep/RNCallKeep.h
+++ ios/RNCallKeep/RNCallKeep.h
@@ -35,6 +35,18 @@ continueUserActivity:(NSUserActivity *)userActivity
                   fromPushKit:(BOOL)fromPushKit
                       payload:(NSDictionary * _Nullable)payload;
 
++ (void)reportNewIncomingCall:(NSString *)uuidString
+                       handle:(NSString *)handle
+                   handleType:(NSString *)handleType
+                     hasVideo:(BOOL)hasVideo
+          localizedCallerName:(NSString * _Nullable)localizedCallerName
+                  fromPushKit:(BOOL)fromPushKit
+                      payload:(NSDictionary * _Nullable)payload
+        withCompletionHandler:(void (^_Nullable)(void))completion;
+
 + (void)endCallWithUUID:(NSString *)uuidString
                  reason:(int)reason;
+
++ (BOOL)isCallActive:(NSString *)uuidString;
+
 @end
